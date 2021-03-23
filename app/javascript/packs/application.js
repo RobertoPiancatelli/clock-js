905f2b28c953880654aa4c38a0c1613e1e7dee86
Roberto Piancatelli <piancatellirob@gmail.com>
2021-03-23 18:31:15 +0000

    added js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,9 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import { clockObject } from '../plugins/clock.js'
+
+document.addEventListener('turbolinks:load', () => {
+    clockObject.start();
+});
---
+++ app/views/clocks/_clock.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! clock, :id, :content, :created_at, :updated_at
+json.url clock_url(clock, format: :json)
